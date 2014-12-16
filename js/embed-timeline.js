d547c40887c370b9a36e0a5f09d783078190f722
Trung Tran <trung@scgstudio.com>
2014-12-16 15:32:40 -0800

    Timeline, Updating time-lapse, and more...
    — Moving timeline images
    — Updating time-lapse for firefox
    — New timeline (User content, Official Content, deleted World )
    — yup! a lot!

--- js/embed-timeline.js
+++ js/embed-timeline.js
@@ -1,8 +1,9 @@
 var timeline_config = {
  width: "100%",
  height: "100%",
- start_zoom_adjust: '5',
- source: 'https://docs.google.com/spreadsheet/pub?key=0Anlb3LlNjL9HdDF1T0tRTEZYeHlYQmxPQzNlb3BfX3c&output=html',
- css:   '../css/timeline.css',		//OPTIONAL PATH TO CSS
- js: 	'../js/timeline.js'	//OPTIONAL
+ start_at_slide:   '1',  //OPTIONAL START AT SPECIFIC SLIDE
+ start_zoom_adjust: '1',
+ source: 'https://docs.google.com/spreadsheet/pub?key=0Anlb3LlNjL9HdFdhWWZjejVmZTlHc1dBTDQ2Q0t1NHc&output=html',
+ css:   '/css/timeline.css',		//OPTIONAL PATH TO CSS
+ js: 	'/js/timeline.js'	//OPTIONAL
 }
---
+++ timeline/timeline-test.md
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Timeline
+permalink: timeline-test.html
+---
+<div id="timeline-embed"></div>
+<script type="text/javascript" src="/js/embed-timeline.js">&nbsp;</script>
+<script type="text/javascript" src="/js/storyjs-embed.js">&nbsp;</script>
+<link charset="utf-8" rel="stylesheet" type="text/css" href="/css/timeline.css">
