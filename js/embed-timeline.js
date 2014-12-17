86d4e6c5e8ede6130d396dd89ee14704d594f81d
Trung Tran <trung@scgstudio.com>
2014-12-17 13:50:01 -0800

    Start slide
    Start slide for timeline

--- js/embed-timeline.js
+++ js/embed-timeline.js
@@ -1,7 +1,7 @@
 var timeline_config = {
  width: "100%",
  height: "100%",
- start_at_slide:   '1',  //OPTIONAL START AT SPECIFIC SLIDE
+ start_at_slide:   '0',  //OPTIONAL START AT SPECIFIC SLIDE
  start_zoom_adjust: '1',
  source: 'https://docs.google.com/spreadsheet/pub?key=0Anlb3LlNjL9HdG1ldFpHbk1OcFV3MWpWSUl0MjNSNHc&output=html',
  css:   '../css/timeline.css',		//OPTIONAL PATH TO CSS
