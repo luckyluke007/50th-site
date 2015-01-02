b52b7becd6dd54bb3809328cfeefe35bbe085660
Trung Tran <trung@scgstudio.com>
2015-01-02 11:42:32 -0800

    Update timeline
    — Switch user and official color
    
    — Add event before 1965

--- js/embed-timeline.js
+++ js/embed-timeline.js
@@ -1,7 +1,7 @@
 var timeline_config = {
  width: "100%",
  height: "100%",
- start_at_slide:   '0',  //OPTIONAL START AT SPECIFIC SLIDE
+ start_at_slide:   '1',  //OPTIONAL START AT SPECIFIC SLIDE
  start_zoom_adjust: '1',
  source: 'https://docs.google.com/spreadsheet/pub?key=0Anlb3LlNjL9HdGRoRjRxMkkza1lfa3pTSVpTaTE4RXc&output=html',
  css:   '../css/timeline.css',		//OPTIONAL PATH TO CSS
